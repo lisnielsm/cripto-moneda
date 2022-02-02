c81a57a977df93b8da08e1741c17c6bfc756c895
Lisniel Sanchez Morales <lisnielsm@gmail.com>
2022-02-02 13:38:18 -0500

    Se creo el custom hook useCriptomoneda y se llamo a la API cryptocompare

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,7 +1,9 @@
 import React, {useEffect, useState} from 'react';
 import styled from '@emotion/styled';
+import axios from 'axios';
 
 import useMoneda from '../hooks/useMoneda';
+import useCriptomoneda from '../hooks/useCriptomoneda';
 
 const Boton = styled.input`
 	margin-top: 20px;
@@ -23,6 +25,9 @@ const Boton = styled.input`
 
 const Formulario = () => {
 
+	// state del listado de criptomonedas
+	const [listacripto, guardarCriptomonedas] = useState([]);
+
 	const MONEDAS = [
 		{ codigo: 'USD', nombre: 'Dolar de Estados Unidos' },
 		{ codigo: 'MXN', nombre: 'Peso Mexicano' },
@@ -32,11 +37,27 @@ const Formulario = () => {
 
 	// ejecutar useMoneda
 	const [moneda, SelectMonedas] = useMoneda('Elige tu moneda', '', MONEDAS);
+
+	// ejecutar useCriptomoneda
+	const [criptomoneda, SelectCripto] = useCriptomoneda('Elige tu criptomoneda', '', listacripto);
+
+	useEffect(() => {
+		const consultarAPI = async () => {
+			const url = 'https://min-api.cryptocompare.com/data/top/mktcapfull?limit=10&tsym=USD';
+
+			const resultado = await axios.get(url);
+
+			guardarCriptomonedas(resultado.data.Data);
+		};
+		consultarAPI();
+	}, []);
 	
 	return ( 
         <form>
 
 			<SelectMonedas />
+
+			<SelectCripto />
 
             <Boton
                 type="submit"
