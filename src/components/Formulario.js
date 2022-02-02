987c2dcdf46a778d66465b0bcf4be3ce2beef1b7
Lisniel Sanchez Morales <lisnielsm@gmail.com>
2022-02-02 13:44:22 -0500

    Se creo el componente Error

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -4,6 +4,7 @@ import axios from 'axios';
 
 import useMoneda from '../hooks/useMoneda';
 import useCriptomoneda from '../hooks/useCriptomoneda';
+import Error from './Error';
 
 const Boton = styled.input`
 	margin-top: 20px;
@@ -27,6 +28,7 @@ const Formulario = () => {
 
 	// state del listado de criptomonedas
 	const [listacripto, guardarCriptomonedas] = useState([]);
+	const [error, guardarError] = useState(false);
 
 	const MONEDAS = [
 		{ codigo: 'USD', nombre: 'Dolar de Estados Unidos' },
@@ -51,9 +53,27 @@ const Formulario = () => {
 		};
 		consultarAPI();
 	}, []);
+
+	// cuando el usuario hace submit
+	const cotizarMoneda = e => {
+		e.preventDefault();
+
+		// validar si ambos campos estan llenos
+		if (moneda.trim() === '' || criptomoneda.trim() === '') {
+			guardarError(true);
+			return;
+		}
+
+		// pasar los datos al componente principal
+		guardarError(false);
+	}
 	
 	return ( 
-        <form>
+		<form
+			onSubmit={cotizarMoneda}
+		>
+
+			{error ? <Error mensaje="Todos los campos son obligatorios" /> : null}
 
 			<SelectMonedas />
 
