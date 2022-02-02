8ad1480a508487bd8343dad297070afcde21a4ba
Lisniel Sanchez Morales <lisnielsm@gmail.com>
2022-02-02 13:51:45 -0500

    Se llamo a la API con la criptomoneda correcta

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from '@emotion/styled';
+import axios from 'axios';
 
 import imagen from './cryptomonedas.png';
 import Formulario from './components/Formulario';
@@ -39,6 +40,29 @@ const Heading = styled.h1`
 
 function App() {
 
+	const [moneda, guardarMoneda] = useState('');
+	const [criptomoneda, guardarCriptomoneda] = useState('');
+	const [resultado, guardarResultado] = useState({});
+
+	useEffect(() => {
+
+		const cotizarCriptomoneda = async () => {
+			// evitamos la ejecucion la primera vez
+			if (moneda === '') return;
+
+			// consultar la api para la cotizacion
+			const url = `https://min-api.cryptocompare.com/data/pricemultifull?fsyms=${criptomoneda}&tsyms=${moneda}`;
+
+			const resultado = await axios.get(url);
+
+			//  guardar cotizacion
+			guardarResultado(resultado.data.DISPLAY[criptomoneda][moneda]);
+		};
+
+		cotizarCriptomoneda();
+
+	}, [moneda, criptomoneda]);
+
 	return (
 		<Contenedor>
 			<div>
@@ -51,7 +75,10 @@ function App() {
 			<div>
 				<Heading>Cotiza Criptomonedas al Instante</Heading>
 
-				<Formulario />
+				<Formulario
+					guardarMoneda={guardarMoneda}
+					guardarCriptomoneda={guardarCriptomoneda}
+				/>
 			</div>
 
 		</Contenedor>
